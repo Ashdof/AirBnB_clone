fea5499361faba43e0d73fe215866c18ea74e049
Ashdof <mzigah4@gmail.com>
2024-03-06 20:54:20 +0000

    Task 1 - created a base class that defines all common attributes for other classes

---
+++ models/base_model.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""A module that defines basic attributes common to all sub-modules"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel
+
+    Description:
+    This class models all attributes are common to other classes
+    inheriting from this.
+    """
+
+    def __init__(self):
+        """Initialize Object
+
+        Description:
+        This attribute initializes the instance object with the id,
+        date created and the date the instance object was updated
+        """
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Save Object
+
+        Description:
+        This method tracks the date the instance object was updated
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Create a Dictionary Representation
+
+        Description:
+        Creates a dictionary representation of this instance object.
+        It contains the class name of the object, the date and time
+        the instance object was created and last updated
+
+        Returns:
+        A dictionary representation of this model instance
+        """
+
+        dict_obj = self.__dict__.copy()
+        dict_obj["__class__"] = self.__class__.__name__
+        dict_obj["created_at"] = self.__dict__["created_at"].isoformat()
+        dict_obj["updated_at"] = self.__dict__["updated_at"].isoformat()
+
+        return dict_obj
+
+    def __str__(self):
+        """
+        A String Representation
+
+        Description:
+        Creates a string representation of this instance object.
+
+        Returns:
+        A string representation of this model instance.
+        """
+
+        info = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return info
