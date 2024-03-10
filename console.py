d743887a709b310701eb62ddbb2b64ef35d6fde3
Ashdof <mzigah4@gmail.com>
2024-03-10 14:33:06 +0000

    Updated console module with commands to create, show and destroy instances of a model

--- console.py
+++ console.py
@@ -2,12 +2,18 @@
 """Module for command line interface implementation"""
 
 import cmd
+import json
+
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Implementation of command line interface"""
 
     prompt = "(hbnb) "
+    cmds_models = ["BaseModel",]
+    cmds = ["create", "show", "update", "destroy", "all"]
 
     def emptyline(self):
         """Pass
@@ -36,6 +42,96 @@ class HBNBCommand(cmd.Cmd):
         """
 
         return True
+
+    def do_create(self, arg_model):
+        """Create New Instance
+
+        Description:
+        Creates a new instance of a model, saves it in the
+        JSON file and prints its id to the console.
+
+        Args:
+        arg_model (model): the type of model to create
+        """
+
+        if not arg_model:
+            print("** class name missing **")
+        elif arg_model not in HBNBCommand.cmds_models:
+            print("** class doesn't exist **")
+        else:
+            dict_models = {"BaseModel": BaseModel}
+            obj_model = dict_models[arg_model]()
+            print(obj_model.id)
+            obj_model.save()
+
+    def do_show(self, args):
+        """Print Instances
+
+        Description:
+        Prints the string representation of an instance based
+        on the model name and id.
+
+        Args:
+        args (model): the model's name and id
+        """
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        arg = args.split(" ")
+
+        if arg[0] not in HBNBCommand.cmds_models:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            all_objs = storage.all()
+
+            for key, value in all_objs.items():
+                obj_name = value.__class__.__name__
+                obj_id = value.id
+
+                if obj_name == arg[0] and obj_id == arg[1].strip('"'):
+                    print(value)
+                    return
+
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        """Delete Instance
+
+        Description:
+        Deletes an instance based on the model's name and id
+
+        Args:
+        args (model): the model's name and id
+        """
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        arg = args.split(" ")
+
+        if arg[0] not in HBNBCommand.cmds_models:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            all_objs = storage.all()
+
+            for key, value in all_objs.items():
+                obj_name = value.__class__.__name__
+                obj_id = value.id
+
+                if obj_name == arg[0] and obj_id == arg[1].strip('"'):
+                    del value
+                    del storage._FileStorage__objects[key]
+                    storage.save()
+                    return
+
+            print("** no instance found **")
 
 
 if __name__ == "__main__":
